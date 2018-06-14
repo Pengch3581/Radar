75f39e6b8635ab670fa445da08558b0fbaeed5b4
pengchao <Pengch3581@outlook.com>
2018-06-14 18:21:38 +0800

    add import alert model

--- agent.py
+++ agent.py
@@ -10,8 +10,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 ALERT_LEVEL = {'P0':['Disk +dmesg +error +on', 'MySQL +is +down +on', 'is +Down', 'Disk +Read-only +on'],
-               'P1':['has +deadlock +on', 'MySQL +has +just +been +restarted', 'has +just +been +restarted'],
-               'P2':['JAVA.rsp_time +is +over', 'BLOCKED +is +over +20 +on', 'webcheck +is +warning +on', 'JAVA.CPU.Usage +is +over', 'gameport +is +warning +on', 'web.test.fail +is +warning +on']}
+               'P1': ['has +deadlock +on', 'MySQL +has +just +been +restarted', 'has +just +been +restarted', 'Free +disk +space +is +less +than +10%', 'tcp_80 +is +down +on', 'FATAL +Error +on'],
+               'P2': ['JAVA.rsp_time +is +over', 'BLOCKED +is +over +20 +on', 'webcheck +is +warning +on', 'JAVA.CPU.Usage +is +over', 'gameport +is +warning +on', 'web.test.fail +is +warning +on', 'Free +disk +space +is +less +than +15%', 'JAVA.OldGC +is +over +92%', 'Network +incoming +traffic +is +over', 'MySQL +CPU +Usage +is +over +600%', 'Time: +Offset +over +30 +second']}
 
 ALERT_LEVEL_USE = {'Disk +dmesg +error':'P0', 'MySQL +is +down':'P0', 'is +DOWN':'P0', 'Disk +Read-only':'P0',
                 'has +deadlock':'P1', 'MySQL +has +just +been +restarted':'P1', 'has +just +been +restarted':'P1',
@@ -198,10 +198,12 @@ dailycount_html = """
                                         <td style="width: 110px; font-weight: bold; font-size: 90%; color: white">服务树异常服</td>
                                         <td style="width: 110px; font-weight: bold; font-size: 90%; color: white">服务树新增</td>
                                         <td style="width: 110px; font-weight: bold; font-size: 90%; color: white">新服清档失败</td>
+                                        <td style="width: 110px; font-weight: bold; font-size: 90%; color: white">审计异常次数</td>
                                     </tr>
                                 </table>
                                 <table cellspacing="4" cellpadding="3" border="0">
                                     <tr>
+                                        <td style="width: 110px; background-color: #32C3D1; font-size: 100%; font-weight: bold; color: white">N/A次</td>
                                         <td style="width: 110px; background-color: #32C3D1; font-size: 100%; font-weight: bold; color: white">N/A次</td>
                                         <td style="width: 110px; background-color: #32C3D1; font-size: 100%; font-weight: bold; color: white">N/A次</td>
                                         <td style="width: 110px; background-color: #32C3D1; font-size: 100%; font-weight: bold; color: white">N/A次</td>
@@ -227,15 +229,42 @@ importalert_content_html = """
                                         <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">影响范围：{1}</td>
                                     </tr>
                                     <tr>
-                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">类型：{2}</td>
-                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">级别：{3}</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">类型：N/A</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">级别：{2}</td>
                                     </tr>
                                     <tr>
-                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">原因：{4}</td>
-                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">时间：{5}</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">状态：{3}</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">时间：{4} {5}</td>
                                     </tr>
                                     <tr>
                                         <td colspan="2" style="width: 500px; font-weight: bold; font-size: 80%; color: #9A9998; border:1px solid #24737B;">描述：{6}</td>
+                                    </tr>
+                                </table>
+                                <table cellspacing="0" cellpadding="0" border="0">
+                                    <tr>
+                                        <td>
+                                            <hr width="600" color="#68686A" />
+                                        </td>
+                                    </tr>
+                                </table>
+"""
+
+importalert_line_html = """
+                                <table cellspacing="0" cellpadding="7" border="0">
+                                    <tr>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">告警：</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">影响范围：</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">类型：</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">级别：</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">状态：</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">时间：</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" style="width: 500px; font-weight: bold; font-size: 80%; color: #9A9998; border:1px solid #24737B;">描述：</td>
                                     </tr>
                                 </table>
                                 <table cellspacing="0" cellpadding="0" border="0">
@@ -259,27 +288,49 @@ nonalert_title_html = """
                                 <table cellspacing="0" cellpadding="7" border="0">
                                     <tr>
                                         <th style="width: 30px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">级别</th>
-                                        <th style="width: 150px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">类型</th>
-                                        <th style="width: 200px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">告警</th>
-                                        <th style="width: 150x; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">主机</th>
+                                        <th style="width: 50px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">类型</th>
+                                        <th style="width: 500px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">告警</th>
+                                        <th style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">主机</th>
                                         <th style="width: 100px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">时间</th>
-                                        <th style="width: 50px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">状态</th>
+                                        <th style="width: 60px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">状态</th>
                                     </tr>
 """
 
 nonalert_content_html = """
                                     <tr>
                                         <td style="width: 30px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{0}</td>
-                                        <td style="width: 150px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{1}</td>
-                                        <td style="width: 200px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{2}</td>
-                                        <td style="width: 150px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{3}</td>
-                                        <td style="width: 100px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{4}</td>
-                                        <td style="width: 50px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{5}</td>
+                                        <td style="width: 50px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">N/A</td>
+                                        <td style="width: 500px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{1}</td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{2}</td>
+                                        <td style="width: 100px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{3}</td>
+                                        <td style="width: 60px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;">{4}</td>
+                                    </tr>
+"""
+
+nonalert_line_html = """
+                                    <tr>
+                                        <td style="width: 30px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;"></td>
+                                        <td style="width: 50px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;"></td>
+                                        <td style="width: 500px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;"></td>
+                                        <td style="width: 300px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;"></td>
+                                        <td style="width: 100px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;"></td>
+                                        <td style="width: 60px; font-weight: bold; font-size: 80%; color: #9A9998; border: 1px solid #24737B;"></td>
                                     </tr>
 """
 
 nonalert_end_html = """
                                 </table>
+"""
+
+new_server_title = """
+                  <!-- 新服模块 -->
+                  <table cellspacing="3" cellpadding="3" border="0">
+                    <tr>
+                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
+                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; margin-top: 15px; font-size: 120%; color: #9A9998; padding-bottom:1px; border-bottom:2px solid #3199A4">新服情况</p>
+                      </td>
+                    </tr>
+                  </table>
 """
 
 end_html = """
@@ -345,7 +396,10 @@ class html_alert():
         # 告警总数
         alert_count = len(problemlist.keys())
         # 未恢复告警数
-        no_resover = len(problemlist.keys()) - len(resovelist.keys())
+        no_resover = 0
+        for i in problemlist.keys():
+            if i not in resovelist.keys():
+                no_resover += 1
         # 高级别告警数
         high_alert = 0
         a = re.compile(create_recomplie(ALERT_LEVEL['P0']))
@@ -354,14 +408,96 @@ class html_alert():
                 high_alert += 1
         return dailycount_html.format(no_resover, high_alert, alert_count)
 
-    def import_alert(self, parameter_list):
+    def import_alert(self, problemlist, resovelist):
         """
         每日重要告警，统计非工作时间内 P0 P1 级别告警
         """
+        import_alert_html = importalert_title_html
         alert_level_P0 = re.compile(create_recomplie(ALERT_LEVEL['P0']))
+        alert_level_P1 = re.compile(create_recomplie(ALERT_LEVEL['P1']))
+        import_count = 0
+        for i in problemlist.keys():
+            if alert_level_P0.search(str(problemlist[i])[1:]):
+                # print(str(problemlist[i])[2:-1].split('#'))
+                # for j in ALERT_LEVEL['P0']:
+                    # a = re.compile(j)
+                    # if a.search(str(problemlist[i])):
+                        # alert_name = j
+                alert_name = str(problemlist[i])[:-1].split('#')[2]
+                alert_range = str(problemlist[i])[:-1].split('#')[1]
+                alert_date = str(problemlist[i])[:-1].split('#')[4]
+                alert_time = str(problemlist[i])[:-1].split('#')[3]
+                alert_description = str(problemlist[i])[:-1].split('#')[0]
+                alert_level = 'P0'
+                if i in resovelist.keys():
+                    alert_status = "已恢复"
+                else:
+                    alert_status = "未恢复"
+                alert_time_compile = re.compile('(0[0123456789]|1[89]|2[0123])\:\d\d\:\d\d')
+                if alert_time_compile.search(alert_time):
+                    import_alert_html += '\n' 
+                    import_alert_html += importalert_content_html.format(
+                        alert_name, alert_range, alert_level, alert_status, alert_date, alert_time, alert_description)
+                    import_count += 1
+            elif alert_level_P1.search(str(problemlist[i])[1:]):
+                # for j in ALERT_LEVEL['P0']:
+                #     a = re.compile(j)
+                #     if a.search(str(problemlist[i])):
+                #         alert_name = j
+                alert_name = str(problemlist[i])[:-1].split('#')[2]
+                alert_range = str(problemlist[i])[:-1].split('#')[1]
+                alert_date = str(problemlist[i])[:-1].split('#')[4]
+                alert_time = str(problemlist[i])[:-1].split('#')[3]
+                alert_description = str(problemlist[i])[:-1].split('#')[0]
+                alert_level = 'P1'
+                if i in resovelist.keys():
+                    alert_status = "已恢复"
+                else:
+                    alert_status = "未恢复"
+                alert_time_compile = re.compile(
+                    '(0[0123456789]|1[89]|2[0123])\:\d\d\:\d\d')
+                if alert_time_compile.search(alert_time):
+                    import_alert_html += '\n' 
+                    import_alert_html += importalert_content_html.format(
+                        alert_name, alert_range, alert_level, alert_status, alert_date, alert_time, alert_description)
+                    import_count += 1
+        if import_count == 0:
+            import_alert_html += '\n' 
+            import_alert_html += importalert_line_html
+        return import_alert_html
+        
 
-    def non_alert(self, parameter_list):
-        pass
+    def non_alert(self, problemlist, resovelist):
+        """
+        未恢复告警模块
+        """
+        non_alert_html = nonalert_title_html + '\n'
+        non_alert_level_P0 = re.compile(create_recomplie(ALERT_LEVEL['P0']))
+        non_alert_level_P1 = re.compile(create_recomplie(ALERT_LEVEL['P1']))
+        non_alert_level_P2 = re.compile(create_recomplie(ALERT_LEVEL['P2']))
+        non_alert_count = 0
+        for i in problemlist.keys():
+            if i not in resovelist.keys():
+                non_alert_status = "未恢复"
+                non_alert_range = str(problemlist[i])[:-1].split('#')[1]
+                non_alert_time = str(problemlist[i])[:-1].split('#')[3]
+                non_alert_name = str(problemlist[i])[:-1].split('#')[2]
+                non_alert_count += 1
+                if re.compile(non_alert_level_P0).search(str(problemlist[i])):
+                    non_alert_level = 'P0'
+                elif re.compile(non_alert_level_P1).search(str(problemlist[i])):
+                    non_alert_level = 'P1'
+                elif re.compile(non_alert_level_P2).search(str(problemlist[i])):
+                    non_alert_level = 'P2'
+                else:
+                    non_alert_level = 'Other'
+                non_alert_html += nonalert_content_html.format(non_alert_level, non_alert_name, non_alert_range, non_alert_time, non_alert_status)
+        if non_alert_count == 0:
+            non_alert_html += nonalert_line_html
+        non_alert_html += nonalert_end_html
+        return non_alert_html
+
+                
     
     def new_server(self, parameter_list):
         pass
@@ -395,7 +531,21 @@ if __name__ == '__main__':
             print(e)
     
     html_all = html_alert()
-    print(html_all.daily_count(problemlist, resovelist))
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    html_total = base_html + '\n' + date_html.format(today) + '\n'
+    html_total += html_all.daily_count(problemlist, resovelist)
+    html_total += '\n' 
+    html_total += html_all.import_alert(problemlist, resovelist)
+    html_total += '\n'
+    html_total += html_all.non_alert(problemlist, resovelist)
+    html_total += '\n'
+    html_total += new_server_title
+    html_total += '\n'
+    html_total += end_html
+    send_mail(html_total)
+
+    
+
     
         
 
