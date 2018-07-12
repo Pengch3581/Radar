783b45139073752f7c3fa167b805a0f67d5c771d
pengchao <Pengch3581@outlook.com>
2018-07-12 11:04:11 +0800

    添加 cron

--- skynet/cron.py
+++ skynet/cron.py
@@ -1,4 +1,6 @@
-from .models import Alerts, Levels, Problems
+import MySQLdb
+import re
+
 
 def daily_report():
   pass
@@ -8,3 +10,100 @@ def weekly_report():
 
 def monthly_report():
     pass
+
+# 收敛模块，定时处理告警表，存入收敛信息表中
+def group_alert():
+    ALERT_WEB = 'webcheck +is +warning'
+    GROUP_ALERT_EXIT = 0
+    db = MySQLdb.connect(host="127.0.0.1", user='root',
+                         port=3306, passwd="123456", db="radar")
+    cursor = db.cursor()
+
+    # 更新收敛表
+    cursor.execute(
+        "select alert_id from skynet_group_alerts where status != '0'")
+    data_group_alert = cursor.fetchall()
+    # group_alertdata is tuple 元组
+    # print(type(group_alertdata))
+    for id in data_group_alert:
+        result = id[0]
+        if result == '':
+                pass
+        else:
+            cursor.execute("SELECT status FROM skynet_alerts WHERE ALERT_ID = '{}'".format(result))
+            alert_status = cursor.fetchall()
+            if len(alert_status) != 1:
+                print("error!")
+                continue
+            sql = "UPDATE skynet_group_alerts SET STATUS = {} WHERE ALERT_ID = '{}'".format(
+                alert_status[0][0], result)
+            try:
+                cursor.execute(sql)
+                db.commit()
+            except:
+                db.rollback()
+
+    # 读取告警表中目前未恢复的告警
+    cursor.execute(
+        "select alert_id,trigger_name,host,datetime,update_time,message from skynet_alerts where status != '0'")
+    data_alert = cursor.fetchall()
+    for i in data_alert:
+        alert_problem = i[1]
+        webcheck_alert = re.compile(ALERT_WEB)
+        # web_testfail = re.compile(ALERT_WEB[0])
+        if webcheck_alert.search(alert_problem):
+            alert_level = 'P1'
+            alert_host = i[2]
+            try:
+                alert_server = i[1].split(' ')[0]
+            except:
+                pass
+            alert_id = i[0]
+            problem_id = 1
+            create_time = i[3]
+            # update_time = NOW()
+            alert_count = 1
+            status = 1
+            # 查询告警是否已经存在，选择 INSERT or UPDATE
+            cursor.execute(
+                "select host,alert_server,alert_count from skynet_group_alerts where status != '0'")
+            date_group_alert = cursor.fetchall()
+            for j in date_group_alert:
+                # 更新
+                print(j)
+                if alert_host == j[0] and alert_host == j[1]:
+                    print('111')
+                    GROUP_ALERT_EXIT = 1
+                    alert_count = str(j[2]) + 1
+                    print(alert_count)
+                    sql = """UPDATE skynet_group_alerts SET update_time=NOW(), alert_count={}""".format(alert_count)
+                    try:
+                        cursor.execute(sql)
+                        db.commit()
+                    except Exception as e:
+                        print(e)
+                        db.rollback()
+                else:
+                    GROUP_ALERT_EXIT = 0
+            # 插入
+            if GROUP_ALERT_EXIT == 0:
+                print('222')
+                sql = """INSERT INTO skynet_group_alerts(
+                    alert_level, host, alert_server, alert_name, alert_id, problem_id, create_time, update_time, alert_count, status) VALUES 
+                    ('{}', '{}', '{}', '{}', '{}', '{}', '{}', NOW(), '{}', '{}')""".format(alert_level, alert_host, alert_server, alert_problem, alert_id, problem_id, create_time, alert_count, status)
+                try:
+                    cursor.execute(sql)
+                    db.commit()
+                except:
+                    db.rollback()
+    db.close()
+    
+
+# 匹配模块，定时匹配收敛模块，匹配成功则添加任务
+def rule_match():
+    pass
+
+# 执行模块，可以使用 api 的方式
+
+if __name__ == '__main__':
+    group_alert()
--- skynet/models.py
+++ skynet/models.py
@@ -28,11 +28,34 @@ class Alerts(models.Model):
         # - 表示逆序
         ordering = ['-datetime']
 
+# 收敛告警表
+class Group_alerts(models.Model):
+    """
+    告警，收敛表
+    status
+    0 已恢复
+    1 未恢复
+    2 已处理
+    """
+    alert_level = models.CharField('告警级别', max_length=10)
+    host = models.CharField('告警主机', max_length=100)
+    alert_server = models.CharField('告警服务', max_length=100)
+    alert_name = models.CharField('告警名称', max_length=200)
+    alert_id = models.CharField('最后告警 id', max_length=100)
+    problem_id = models.CharField('告警项 id', max_length=100)
+    create_time = models.CharField('告警录入时间', max_length=200)
+    update_time = models.CharField('告警更新时间', max_length=200)
+    alert_count = models.CharField('告警次数', max_length=100)
+    status = models.CharField('告警状态', max_length=100)
+
+    def __str__(self):
+        return self.alert_name
+
 class Problems(models.Model):
     '''
     告警项
     '''
-    problem_name = models.CharField('告警项', max_length=50)
+    problem_name = models.CharField('告警项', max_length=200)
     level = models.CharField('告警登记', max_length=10)
 
     def __str__(self):
@@ -41,6 +64,10 @@ class Problems(models.Model):
 class Levels(models.Model):
     '''
     告警级别
+    P0：整机硬件级别不可用
+    P1：服务不可用，整机不可用隐患
+    P2：体验性告警，服务不可用隐患
+    Other：其他
     '''
     level_id = models.CharField('告警级别', max_length=10)
 
