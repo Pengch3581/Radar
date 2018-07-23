4f4f87a28fdf8be6d5fd50b6e7505efaf40c5ee8
pengchao <Pengch3581@outlook.com>
2018-07-23 14:51:25 +0800

    modify redis db

--- skynet/set_redis.py
+++ skynet/set_redis.py
@@ -5,5 +5,6 @@ import sys
 name = sys.argv[1]
 subject = sys.argv[2]
 message = sys.argv[3]
-r = redis.StrictRedis(host='127.0.0.1', port=6379)
+redis_db=sys.argv[4]
+r = redis.StrictRedis(host='127.0.0.1', port=6379, db=redis_db)
 r.set(subject, message)
